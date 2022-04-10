f671b0a9441c076011aac46da48e61b07145c042
GoergPflug <103200013+GoergPflug@users.noreply.github.com>
2022-04-10 11:10:13 +0200

    support for C++ and Arduino IDE

--- tiny_multi_os.h
+++ tiny_multi_os.h
@@ -1,4 +1,4 @@
-/* CPKI AttinyGfxApi & TinyMultiOs, Preview Version 0.2.1
+/* CPKI AttinyGfxApi & TinyMultiOs, Preview Version 0.3
 see
 https://www.youtube.com/watch?v=WNJQXsJqSbM
 Copyright (c) 2002
@@ -12,6 +12,8 @@ THIS SOFTWARE IS PROVIDED BY THE Görg Pflug, CPKI Gmbh AND CONTRIBUTORS “AS I
 
 
 Version:
+0.3		Compatible with C++, compiles in Microchip Studio and Arduino, most samples still untested
+		added .INO example for Arduino
 0.2		Multiple Consoles
 0.2.1	ASCII Font Support
 		define: ENABLE_FONT_BASIC
@@ -21,6 +23,15 @@ Version:
 		
 */
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/boot.h>
+#include <avr/interrupt.h>
+#include <string.h>
 
 
 #define s8 signed char
@@ -64,7 +75,7 @@ static void os_i2c_write (const u8 *buf, u8 len);
 
 #ifdef ENABLE_CONSOLE
 
-__flash const u8 os_font[] = {
+const u8 os_font[] __attribute__((progmem)) = {
 #ifdef ENABLE_FONT_BASIC
 // source https://raw.githubusercontent.com/dhepper/font8x8/master/font8x8_basic.h , public domain
 0,0,0,0,0,0,0,0,0,0,6,95,95,6,0,0,0,3,3,0,3,3,0,0,20,127,127,20,127,127,20,0,36,46,107,107,58,18,0,0,70,102,48,24,12,102,98,0,48,122,79,93,55,122,72,0,4,7,3,0,0,0,0,0,0,28,62,99,65,0,0,0,0,65,99,62,28,0,0,0,8,42,62,28,28,62,42,8,8,8,62,62,8,8,0,0,0,128,224,96,0,0,0,0,8,8,8,8,8,8,0,0,0,0,96,96,0,0,0,0,96,48,24,12,6,3,1,0,62,127,113,89,77,127,62,0,64,66,127,127,64,64,0,0,98,115,89,73,111,102,0,0,34,99,73,73,127,54,0,0,24,28,22,83,127,127,80,0,39,103,69,69,125,57,0,0,60,126,75,73,121,48,0,0,3,3,113,121,15,7,0,0,54,127,73,73,127,54,0,0,6,79,73,105,63,30,0,0,0,0,102,102,0,0,0,0,0,128,230,102,0,0,0,0,8,28,54,99,65,0,0,0,36,36,36,36,36,36,0,0,0,65,99,54,28,8,0,0,2,3,81,89,15,6,0,0,62,127,65,93,93,31,30,0,124,126,19,19,126,124,0,0,65,127,127,73,73,127,54,0,28,62,99,65,65,99,34,0,65,127,127,65,99,62,28,0,65,127,127,73,93,65,99,0,65,127,127,73,29,1,3,0,28,62,99,65,81,115,114,0,127,127,8,8,127,127,0,0,0,65,127,127,65,0,0,0,48,112,64,65,127,63,1,0,65,127,127,8,28,119,99,0,65,127,127,65,64,96,112,0,127,127,14,28,14,127,127,0,127,127,6,12,24,127,127,0,28,62,99,65,99,62,28,0,65,127,127,73,9,15,6,0,30,63,33,113,127,94,0,0,65,127,127,9,25,127,102,0,38,111,77,89,115,50,0,0,3,65,127,127,65,3,0,0,127,127,64,64,127,127,0,0,31,63,96,96,63,31,0,0,127,127,48,24,48,127,127,0,67,103,60,24,60,103,67,0,7,79,120,120,79,7,0,0,71,99,113,89,77,103,115,0,0,127,127,65,65,0,0,0,1,3,6,12,24,48,96,0,0,65,65,127,127,0,0,0,8,12,6,3,6,12,8,0,128,128,128,128,128,128,128,128,0,0,3,7,4,0,0,0,32,116,84,84,60,120,64,0,65,127,63,72,72,120,48,0,56,124,68,68,108,40,0,0,48,120,72,73,63,127,64,0,56,124,84,84,92,24,0,0,72,126,127,73,3,2,0,0,152,188,164,164,248,124,4,0,65,127,127,8,4,124,120,0,0,68,125,125,64,0,0,0,96,224,128,128,253,125,0,0,65,127,127,16,56,108,68,0,0,65,127,127,64,0,0,0,124,124,24,56,28,124,120,0,124,124,4,4,124,120,0,0,56,124,68,68,124,56,0,0,132,252,248,164,36,60,24,0,24,60,36,164,248,252,132,0,68,124,120,76,4,28,24,0,72,92,84,84,116,36,0,0,0,4,62,127,68,36,0,0,60,124,64,64,60,124,64,0,28,60,96,96,60,28,0,0,60,124,112,56,112,124,60,0,68,108,56,16,56,108,68,0,156,188,160,160,252,124,0,0,76,100,116,92,76,100,0,0,8,8,62,119,65,65,0,0,0,0,0,119,119,0,0,0,65,65,119,62,8,8,0,0,2,3,1,3,2,3,1,0,0,0,0,0,0,0,0,0,
@@ -170,14 +181,14 @@ FUSES =
 	.extended = FUSE_SELFPRGEN,
 };
 */
-__flash const u8 os_conv_hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+const u8 os_conv_hex[] __attribute__((progmem)) = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 void ApiIntToHex(u16 in, u8 *out)
 {
-	out[0] = os_conv_hex[(in >> 12) & 0xf];
-	out[1] = os_conv_hex[(in >> 8) & 0xf];
-	out[2] = os_conv_hex[(in >> 4) & 0xf];
-	out[3] = os_conv_hex[in & 0xf];
+	out[0] =pgm_read_byte(&os_conv_hex[(in >> 12) & 0xf]);
+	out[1] =pgm_read_byte(&os_conv_hex[(in >> 8) & 0xf]);
+	out[2] =pgm_read_byte(&os_conv_hex[(in >> 4) & 0xf]);
+	out[3] =pgm_read_byte(&os_conv_hex[in & 0xf]);
 }
 
 u8 ApiCharToFontIndex(u8 c)
@@ -192,7 +203,7 @@ u8 ApiCharToFontIndex(u8 c)
 	#endif
 }
 
-extern void SystemServer_WriteToScreen(u8 *txt, u8 *screen, u8 x, u8 y)
+extern void SystemServer_WriteToScreen(const char *txt, u8 *screen, u8 x, u8 y)
 {
 	int p = x + 16 * y;
 	int l = strlen(txt);
@@ -319,9 +330,8 @@ void os_init_ssd1306 (void)
 	os_i2c_write(init1306, sizeof(init1306));
 }
 
+#ifndef ENABLE_ARDUINO_SUPPORT
 extern void MainTask();
-
-
 int main()
 {
 	cli();
@@ -329,3 +339,10 @@ int main()
 	os_init_ssd1306();
 	MainTask();
 }
+#else
+int streamgfx_init_arduino()
+{
+	os_i2c_init();
+	os_init_ssd1306();
+}
+#endif
