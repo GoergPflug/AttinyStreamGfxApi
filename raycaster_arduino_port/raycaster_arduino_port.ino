acb23de7c66bf2f6e4781585d9edf09f143b49f6
GoergPflug <103200013+GoergPflug@users.noreply.github.com>
2022-04-11 07:36:37 +0200

    port of the raycaster to arduino with wire

---
+++ raycaster_arduino_port/raycaster_arduino_port.ino
@@ -0,0 +1,330 @@
+// to get it running: change the #include "/dev/tiny_multi_os.h" and #include "/dev/displayfunc.h" to the place where you stored these files!
+// change #define WIRE_SCREEN_ADDRESS if your Display uses a different I2C address
+// this suffers a bit on the arduino, because there is no support for fixed point, so it seems it is actually running faster on the attiny in embedded c.
+
+
+#include <Wire.h>
+
+
+#define ENABLE_ARDUINO_SUPPORT
+#define ENABLE_WIRE
+#define WIRE_SCREEN_ADDRESS 0x3C
+
+#include <stdlib.h>
+#include <stdfix.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/boot.h>
+#include <avr/interrupt.h>
+#include <string.h>
+///////////////////////////////////////////////////////////////////////////////
+// OS-Configuration////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+//#define ENABLE_MULTITASK
+#define OS_MAX_TASKS 2
+#define OS_TASK_STACK 384
+//#define ENABLE_USERFONT 1
+
+///////////////////////////////////////////////////////////////////////////////
+//Gfx-Api Configuration
+///////////////////////////////////////////////////////////////////////////////
+unsigned char render_lines(unsigned char x,unsigned char y);
+#define DISPLAYFUNC Display
+#define CONSOLE_LINE_END (CONSOLE_LINE_START+(CONSOLE_SIZE_Y*8))
+//#define ENABLE_SPRITES
+#define PIXEL_CALLBACK render_lines
+//#define ENABLE_LAYERS
+//#define ENABLE_CONSOLE
+#define NR_LAYERS 1
+#define NR_SPRITES 0
+//#define SUBTRACT_LAYER0
+#define CONSOLE_SIZE_Y 1
+#define CONSOLE_LINE_START (0*8)
+////////
+#include "/dev/tiny_multi_os.h"
+///////////////////////////////////////////////////////////////////////////////
+//Gfx-Api Build Render Function from Defines
+#include "/dev/displayfunc.h"
+/////////////////////////////////////////////////////////////////////
+// this is NOT how you should design a 3D-Engine, but it demonstrates how to draw lines
+
+
+#pragma GCC optimize ("Ofast","unsafe-math-optimizations")
+
+
+
+// Port of: https://lodev.org/cgtutor/raycasting.html#Untextured_Raycaster_
+
+/*
+Copyright (c) 2004-2021, Lode Vandevenne
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+static float posX = 22, posY = 12;  //x and y start position
+static float dirX = -1, dirY = 0; //initial direction vector
+static float planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+
+#define screenWidth 128
+#define screenHeight 64
+#define mapWidth 24
+#define mapHeight 24
+
+static const u8 worldMap[mapWidth][mapHeight] =
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+
+static void cast_ray(int x, unsigned char *c, unsigned char *heigt_out)
+{
+
+  float time = 0; //time of current frame
+  float oldTime = 0; //time of previous frame
+  //while (1)
+  {
+    int w = 128;
+    static const float m_inv=1.0f/(127.0);
+    //for (int x = 0; x < 128; x++)
+    {
+      //calculate ray position and direction
+      float cameraX = 2 * x *m_inv; //x-coordinate in camera space
+      float rayDirX = dirX + planeX * cameraX;
+      float rayDirY = dirY + planeY * cameraX;
+      //which box of the map we're in
+      int mapX = posX;
+      int mapY = posY;
+
+      //length of ray from current position to next x or y-side
+      float sideDistX;
+      float sideDistY;
+
+      //length of ray from one x or y-side to next x or y-side
+      //these are derived as:
+      //deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX))
+      //deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY))
+      //which can be simplified to abs(|rayDir| / rayDirX) and abs(|rayDir| / rayDirY)
+      //where |rayDir| is the length of the vector (rayDirX, rayDirY). Its length,
+      //unlike (dirX, dirY) is not 1, however this does not matter, only the
+      //ratio between deltaDistX and deltaDistY matters, due to the way the DDA
+      //stepping further below works. So the values can be computed as below.
+      // Division through zero is prevented, even though technically that's not
+      // needed in C++ with IEEE 754 floating point values.
+      float deltaDistX = fabs(1 / rayDirX);
+      float deltaDistY = fabs(1 / rayDirY);
+
+      float perpWallDist;
+
+      //what direction to step in x or y-direction (either +1 or -1)
+      int stepX;
+      int stepY;
+
+      int hit = 0; //was there a wall hit?
+      int side; //was a NS or a EW wall hit?
+      //calculate step and initial sideDist
+      if (rayDirX < 0)
+      {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+      }
+      else
+      {
+        stepX = 1;
+        sideDistX = (mapX + 1.0f - posX) * deltaDistX;
+      }
+      if (rayDirY < 0)
+      {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+      }
+      else
+      {
+        stepY = 1;
+        sideDistY = (mapY + 1.0f - posY) * deltaDistY;
+      }
+      //perform DDA
+      while (hit == 0)
+      {
+        //jump to next map square, either in x-direction, or in y-direction
+        if (sideDistX < sideDistY)
+        {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else
+        {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0) hit = 1;
+      }
+      //Calculate distance projected on camera direction. This is the shortest distance from the point where the wall is
+      //hit to the camera plane. Euclidean to center camera point would give fisheye effect!
+      //This can be computed as (mapX - posX + (1 - stepX) / 2) / rayDirX for side == 0, or same formula with Y
+      //for size == 1, but can be simplified to the code below thanks to how sideDist and deltaDist are computed:
+      //because they were left scaled to |rayDir|. sideDist is the entire length of the ray above after the multiple
+      //steps, but we subtract deltaDist once because one step more into the wall was taken above.
+      if (side == 0) perpWallDist = (sideDistX - deltaDistX);
+      else          perpWallDist = (sideDistY - deltaDistY);
+
+      int h = 64;
+
+      //Calculate height of line to draw on screen
+      int lineHeight = (int)(h / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + h / 2;
+      if (drawStart < 0) drawStart = 0;
+      int drawEnd = lineHeight / 2 + h / 2;
+      if (drawEnd >= h) drawEnd = h - 1;
+
+      //choose wall color
+      unsigned char color;
+      switch (worldMap[mapX][mapY])
+      {
+      case 1:  color = 20;    break; //red
+      case 2:  color = 30;  break; //green
+      case 3:  color = 40;   break; //blue
+      case 4:  color = 50;  break; //white
+      default: color = 60; break; //yellow
+      }
+    //  color *= 4;
+    *c=color;
+      //give x and y sides different brightness
+      if (side == 1) { color = color / 2; }
+
+      //draw the pixels of the stripe as a vertical line
+    *heigt_out=lineHeight;
+      
+//      vline(x, drawStart, lineHeight, color);
+    //xxx verLine(x, drawStart, drawEnd, color);
+    }
+    //timing for input and FPS counter
+/*
+    if (keyDown(SDLK_UP))
+    {
+      if (worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
+      if (worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+    }
+    //move backwards if no wall behind you
+    if (keyDown(SDLK_DOWN))
+    {
+      if (worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
+      if (worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+    }
+    //rotate to the right
+    if (keyDown(SDLK_RIGHT))
+    {
+      //both camera direction and camera plane must be rotated
+      float oldDirX = dirX;
+      dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+      dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+      float oldPlaneX = planeX;
+      planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+      planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+    }
+    */
+    //rotate to the left
+    //float rotSpeed = 0.1;
+    /*if (1)
+    {
+      //both camera direction and camera plane must be rotated
+      float oldDirX = dirX;
+      dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+      dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+      float oldPlaneX = planeX;
+      planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+      planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+    }*/
+  }
+  //return 0;
+}
+
+
+unsigned char render_lines(unsigned char x,unsigned char y)
+{
+  static unsigned char c,h;
+  if(y==0)cast_ray(x,&c,&h);
+  if(y>(32-h)&&(y<32+h))return c;
+  return 0;
+}
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  streamgfx_init_arduino();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (;;)
+  {
+    Display(0, 0, 0,0);
+    
+    float rotSpeed = 0.1;
+    //both camera direction and camera plane must be rotated
+    float oldDirX = dirX;
+    dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+    dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+    float oldPlaneX = planeX;
+    planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+    planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+
+
+  }
+
+}
