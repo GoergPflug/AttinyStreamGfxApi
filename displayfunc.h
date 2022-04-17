ba5406296b97b8227952cbaa1a4f40d3bae36a19
GoergPflug <103200013+GoergPflug@users.noreply.github.com>
2022-04-17 09:42:13 +0200

    faster triangles

--- displayfunc.h
+++ displayfunc.h
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-/* CPKI AttinyStreamGfxApi & TinyMultiOs, Version 0.8
+/* CPKI AttinyStreamGfxApi & TinyMultiOs, Version 0.8.1
 see
 https://www.youtube.com/watch?v=WNJQXsJqSbM
 Copyright (c) 2002
@@ -13,6 +13,8 @@ All advertising materials mentioning features or use of this software must displ
 Neither the name of the Cpki GmbH nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY Görg Pflug, CPKI Gmbh AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 Version:
+0.8.1	faster
+0.8		Filled Triangle drawing
 0.5		faster, support functions, Consistent API, engine-interleaved I2C, propper line-drawing-api
 		much better 3D-Performance
 0.4		Prelimary Support for Arduino-Wire, performance worse than on attiny85
@@ -112,79 +114,140 @@ static const u8 hl[8] PROGMEM ={1,1+2,1+2+4,1+2+4+8,1+2+4+16,1+2+4+8+16+32,1+2+4
 }
 
 #ifdef ENABLE_TRIANGLES
-// triangle code based on https://www.avrfreaks.net/sites/default/files/triangles.c , https://www.avrfreaks.net/forum/algorithm-draw-filled-triangle
+// triangle code was once based on https://www.avrfreaks.net/sites/default/files/triangles.c , https://www.avrfreaks.net/forum/algorithm-draw-filled-triangle
 
 void fillTriangle(u8 x1,u8 y1,u8 x2,u8 y2,u8 x3,u8 y3, u8 c,u8*linebuffer) {
 	u8 t1x,t2x,y,minx,maxx,t1xp,t2xp;
-	bool changed1 = false;
-	bool changed2 = false;
+	u8 change=0;
+	
+	minx=x1;
+	if(x2<minx)minx=x2;
+	if(x3<minx)minx=x3;
+	maxx=x1;
+	if(x2>maxx)maxx=x2;
+	if(x3>maxx)maxx=x3;
+	if((minx&0xf8)>_cur_seg+7) return ; // links grösser als cur seg... raus...
+	if((maxx&0xf8)<_cur_seg) return ; // rechts ist kleiner als das akute segment....raus...die linie
+
+
 	s8 signx1,signx2,dx1,dy1,dx2,dy2;
 	u8 e1,e2;
 	// Sort vertices
-	if (y1>y2) { swapu8(y1,y2); swapu8(x1,x2); }
-	if (y1>y3) { swapu8(y1,y3); swapu8(x1,x3); }
-	if (y2>y3) { swapu8(y2,y3); swapu8(x2,x3); }
+	if (y1>y2) 
+	{ 
+		swapu8(y1,y2); 
+		swapu8(x1,x2); 
+	}
+	if (y1>y3) 
+	{ 
+		swapu8(y1,y3); 
+		swapu8(x1,x3); 
+	}
+	if (y2>y3) 
+	{ 
+		swapu8(y2,y3); 
+		swapu8(x2,x3); 
+	}
 
-	t1x=t2x=x1; y=y1;   // Starting points
+	t1x=x1;
+	t2x=x1; 
+	
+	y=y1;   // Starting points
 
-	dx1 = (s8)(x2 - x1); if(dx1<0) { dx1=-dx1; signx1=-1; } else signx1=1;
+	dx1 = (s8)(x2 - x1); 
+	if(dx1<0) 
+	{ 
+		dx1=-dx1; 
+		signx1=-1; 
+	} 
+	else 
+		signx1=1;
 	dy1 = (s8)(y2 - y1);
 	
-	dx2 = (s8)(x3 - x1); if(dx2<0) { dx2=-dx2; signx2=-1; } else signx2=1;
+	dx2 = (s8)(x3 - x1); 
+	if(dx2<0) 
+	{ 
+		dx2=-dx2; 
+		signx2=-1; 
+	} 
+	else signx2=1;
+	
 	dy2 = (s8)(y3 - y1);
 	
-	if (dy1 > dx1) {   // swap values
+	if (dy1 > dx1) 
+	{   // swap values
 		swapu8(dx1,dy1);
-		changed1 = true;
+		change|=1;// = true;
 	}
-	if (dy2 > dx2) {   // swap values
+	if (dy2 > dx2) 
+	{   // swap values
 		swapu8(dy2,dx2);
-		changed2 = true;
+		change|=2;// = true;
 	}
 	
 	e2 = (u8)(dx2>>1);
 	// Flat top, just process the second half
 	if(y1==y2) goto next;
+	
 	e1 = (u8)(dx1>>1);
 	
-	for (u8 i = 0; i < dx1;) {
+	for (u8 i = 0; i < dx1;) 
+	{
 		t1xp=0; t2xp=0;
-		if(t1x<t2x) { minx=t1x; maxx=t2x; }
-		else		{ minx=t2x; maxx=t1x; }
+		if(t1x<t2x) 
+		{ 
+			minx=t1x; 
+			maxx=t2x; 
+		}
+		else		
+		{ 
+			minx=t2x; 
+			maxx=t1x; 
+		}
 		// process first line until y value is about to change
 		while(i<dx1) 
 		{
 			i++;
 			e1 += dy1;
-			while (e1 >= dx1) {
+			while (e1 >= dx1) 
+			{
 				e1 -= dx1;
-				if (changed1) t1xp=signx1;//t1x += signx1;
-				else          goto next1;
+				if (change&1) 
+					t1xp=signx1;
+				else          
+					goto next1;
 			}
-			if (changed1) break;
-			else t1x += signx1;
+			if (change&1) break;
+			
+			t1x += signx1;
 		}
 		// Move line
 		next1:
 		// process second line until y value is about to change
-		while (1) {
+		while (1) 
+		{
 			e2 += dy2;
 			while (e2 >= dx2) {
 				e2 -= dx2;
-				if (changed2) t2xp=signx2;//t2x += signx2;
-				else          goto next2;
+				if (change&2) 
+					t2xp=signx2;
+				else          
+					goto next2;
 			}
-			if (changed2)     break;
-			else              t2x += signx2;
+			if (change&2)
+				break;
+			t2x += signx2;
 		}
 		next2:
-		if(minx>t1x) minx=t1x; if(minx>t2x) minx=t2x;
-		if(maxx<t1x) maxx=t1x; if(maxx<t2x) maxx=t2x;
+		if(minx>t1x) minx=t1x; 
+		if(minx>t2x) minx=t2x;
+		if(maxx<t1x) maxx=t1x; 
+		if(maxx<t2x) maxx=t2x;
 		_hline(minx,y, maxx, y,c,linebuffer);    // Draw line from min to max points found on the y
 		// Now increase y
-		if(!changed1) t1x += signx1;
+		if(!(change&1)) t1x += signx1;
 		t1x+=t1xp;
-		if(!changed2) t2x += signx2;
+		if(!(change&2)) t2x += signx2;
 		t2x+=t2xp;
 		y += 1;
 		if(y==y2) break;
@@ -192,64 +255,96 @@ void fillTriangle(u8 x1,u8 y1,u8 x2,u8 y2,u8 x3,u8 y3, u8 c,u8*linebuffer) {
 	}
 	next:
 	// Second half
-	dx1 = (s8)(x3 - x2); if(dx1<0) { dx1=-dx1; signx1=-1; } else signx1=1;
+	dx1 = (s8)(x3 - x2); 
+	if(dx1<0) 
+	{ 
+		dx1=-dx1; 
+		signx1=-1; 
+	} 
+	else 
+		signx1=1;
 	dy1 = (s8)(y3 - y2);
 	t1x=x2;
 	
-	if (dy1 > dx1) {   // swap values
+	if (dy1 > dx1) 
+	{   // swap values
 		swapu8(dy1,dx1);
-		changed1 = true;
-	} else changed1=false;
+		change|=1;// = true;
+	} 
+	else 
+		change&=2;//false;
 	
 	e1 = (u8)(dx1>>1);
 	
-	for (u8 i = 0; i<=dx1; i++) {
+	for (u8 i = 0; i<=dx1; i++) 
+	{
 		t1xp=0; t2xp=0;
-		if(t1x<t2x) { minx=t1x; maxx=t2x; }
-		else		{ minx=t2x; maxx=t1x; }
+		if(t1x<t2x) 
+		{ 
+			minx=t1x; 
+			maxx=t2x; 
+		}
+		else		
+		{ 
+			minx=t2x; 
+			maxx=t1x; 
+		}
 		// process first line until y value is about to change
 		while(i<dx1) {
 			e1 += dy1;
-			while (e1 >= dx1) {
+			while (e1 >= dx1) 
+			{
 				e1 -= dx1;
-				if (changed1) { t1xp=signx1; break; }//t1x += signx1;
-				else          goto next3;
+				if (change&1) 
+				{ 
+					t1xp=signx1; 
+					break; 
+				}
+				else          
+					goto next3;
 			}
-			if (changed1) break;
-			else   	   	  t1x += signx1;
+			if (change&1) 
+				break;
+			
+			t1x += signx1;
 			if(i<dx1) i++;
 		}
 		next3:
 		// process second line until y value is about to change
-		while (t2x!=x3) {
+		while (t2x!=x3) 
+		{
 			e2 += dy2;
-			while (e2 >= dx2) {
+			while (e2 >= dx2) 
+			{
 				e2 -= dx2;
-				if(changed2) t2xp=signx2;
-				else          goto next4;
+				if(change&2) 
+					t2xp=signx2;
+				else          
+					goto next4;
 			}
-			if (changed2)     break;
-			else              t2x += signx2;
+			if (change&2)     
+				break;
+			
+			t2x += signx2;
 		}
 		next4:
 
-		if(minx>t1x) minx=t1x; if(minx>t2x) minx=t2x;
-		if(maxx<t1x) maxx=t1x; if(maxx<t2x) maxx=t2x;
+		if(minx>t1x) minx=t1x; 
+		if(minx>t2x) minx=t2x;
+		if(maxx<t1x) maxx=t1x; 
+		if(maxx<t2x) maxx=t2x;
 		_hline(minx,y, maxx, y,c,linebuffer);    // Draw line from min to max points found on the y
 		// Now increase y
-		if(!changed1) t1x += signx1;
+		if(!(change&1)) t1x += signx1;
 		t1x+=t1xp;
-		if(!changed2) t2x += signx2;
+		if(!(change&2)) t2x += signx2;
 		t2x+=t2xp;
 		y += 1;
 		if(y>y3) return;
 	}
 }
 #endif
-
 #endif
-
-
 #endif
 
 static void DISPLAYFUNC (
@@ -315,7 +410,6 @@ static void DISPLAYFUNC (
 #endif
 	)
 	{
-	
 #ifdef ENABLE_LINEDRAWING
 	   u8 line_cur_bit = 1<<(x_pos_screen&7);
 	   _cur_seg=x_pos_screen&0xf8;
@@ -333,7 +427,7 @@ static void DISPLAYFUNC (
 	  		   _line(_gfx_points_of_lines[i], _gfx_points_of_lines[i+1], _gfx_points_of_lines[i+2], _gfx_points_of_lines[i+3],linebuffer);
 #ifdef ENABLE_TRIANGLES
 		   for(u8 i=_gfx_linepos;i<_gfx_linepos+_gfx_tripos;i+=8)
- 		   fillTriangle(_gfx_points_of_lines[i], _gfx_points_of_lines[i+1], _gfx_points_of_lines[i+2], _gfx_points_of_lines[i+3],_gfx_points_of_lines[i+4], _gfx_points_of_lines[i+5],_gfx_points_of_lines[i+6],linebuffer);
+	 		   fillTriangle(_gfx_points_of_lines[i], _gfx_points_of_lines[i+1], _gfx_points_of_lines[i+2], _gfx_points_of_lines[i+3],_gfx_points_of_lines[i+4], _gfx_points_of_lines[i+5],_gfx_points_of_lines[i+6],linebuffer);
 #endif		 	   
 	   }
 #endif
@@ -456,8 +550,6 @@ static void DISPLAYFUNC (
 #define STRINGIZE(A) STRINGIZE_NX(A)
 				asm("cbi 0x18," STRINGIZE(PIN_SCL));
 				asm("sbi 0x0d,1");
-#undef STRINGNIZE
-#undef STRINGNIZE_NX
 // 				PORTB &= ~_BV(PIN_SCL);
 // 				USICR |= _BV(USICLK);
  			}
@@ -545,4 +637,7 @@ static void DISPLAYFUNC (
 #ifdef DISABLE_HALFTONE
 #undef DISABLE_HALFTONE
 #endif
+#undef STRINGNIZE
+#undef STRINGNIZE_NX
+
 #pragma GCC pop_options
